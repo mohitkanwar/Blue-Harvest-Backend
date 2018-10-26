9c7795736d3a864a3d87885396656f7b7de2fb91
Mohit Kanwar <m.m.kanwar@gmail.com>
2018-10-26 21:33:47 +0530

    deleting moved classes

--- src/main/java/com/mk/blueharvest/backendassignment/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mk.blueharvest.backendassignment.repositories;
-
-import com.mk.blueharvest.backendassignment.entities.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
-}
