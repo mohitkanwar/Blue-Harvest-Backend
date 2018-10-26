91c03fc2b37f7f416c3f7ae4efc28b044eb00d64
Mohit Kanwar <m.m.kanwar@gmail.com>
2018-10-26 21:28:30 +0530

    Accounts Service added

--- src/main/java/com/mk/blueharvest/backendassignment/account/services/AccountsService.java
+++ src/main/java/com/mk/blueharvest/backendassignment/account/services/AccountsService.java
@@ -1,5 +1,12 @@
 package com.mk.blueharvest.backendassignment.account.services;
 
+import com.mk.blueharvest.backendassignment.account.entities.Account;
+
+import java.util.List;
+
 public interface AccountsService {
 
+    void save(Account account);
+
+    List<Account> getAllAccounts();
 }
--- src/test/java/com/mk/blueharvest/backendassignment/account/services/AccountsServiceIntegrationTest.java
+++ src/test/java/com/mk/blueharvest/backendassignment/account/services/AccountsServiceIntegrationTest.java
@@ -1,10 +1,15 @@
 package com.mk.blueharvest.backendassignment.account.services;
 
+import com.mk.blueharvest.backendassignment.account.entities.Account;
+import com.mk.blueharvest.backendassignment.account.util.AccountType;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,7 +17,13 @@ public class AccountsServiceIntegrationTest {
     @Autowired
     private AccountsService accountsService;
     @Test
-    public void justForLettingTheTestPass(){
+    public void testAccountSaving(){
+        Account account = new Account();
+        account.setBalance(200);
+        account.setAccountType(AccountType.CURRENT);
+        accountsService.save(account);
+        List<Account> accountList = accountsService.getAllAccounts();
+        Assert.assertTrue(accountList.contains(account));
 
     }
 }
